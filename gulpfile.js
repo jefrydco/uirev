7b4c9429cbf4228a86ebd445707145459452d493
Jefry Dewangga <jdewangga7@gmail.com>
2017-01-08 17:07:32 +0700

    Make some improvements

--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,16 @@ const   autoprefixer        = require('gulp-autoprefixer'),
         browserSync         = require('browser-sync').create(),
         cache               = require('gulp-cache'),
         clean               = require('gulp-clean'),
-        concat              = require('gulp-concat'),
+        concatJS            = require('gulp-concat'),
         gulp                = require('gulp'),
         imagemin            = require('gulp-imagemin'),
         imageminGiflosy     = require('imagemin-giflossy'),
         imageminJpgtran     = require('imagemin-jpegtran'),
         imageminPngquant    = require('imagemin-pngquant'),
         imageminSvgo        = require('imagemin-svgo'),
-        livereload          = require('gulp-livereload'),
-        minifyCSS            = require('gulp-clean-css'),
+        minifyCSS           = require('gulp-clean-css'),
         pug                 = require('gulp-pug'),
-        pugLint             = require('gulp-pug-lint'),
+        rename              = require('gulp-rename'),
         scss                = require('gulp-sass'),
         sourcemaps          = require('gulp-sourcemaps'),
         uglify              = require('gulp-uglify')
@@ -25,9 +24,30 @@ const   autoprefixer        = require('gulp-autoprefixer'),
 const src = {
     fontSource: './src/assets/scss/fonts/*',
     imgSource: './src/assets/img/*',
-    jsSource: ['./src/assets/js/vendor/jquery.min.js', './src/assets/js/vendor/bootstrap.min.js', './src/assets/js/*.js'],
+    jsSourceBuild: [
+        './src/assets/js/vendor/jquery.min.js',
+        './src/assets/js/vendor/bootstrap.min.js',
+        './src/assets/js/main.js'
+    ],
+    jsSourceDev: [
+        './src/assets/js/main.js',
+        '!./src/assets/vendor/*.js'
+    ],
+    jsSourceDevVendor: [
+        './src/assets/js/vendor/jquery.min.js',
+        './src/assets/js/vendor/bootstrap.min.js'
+    ],
     pugSource: './src/pug/index.pug',
-    sassSource: ['./src/assets/scss/vendor/*.scss', './src/assets/scss/*.scss']
+    sassSourceBuild: [
+        './src/assets/scss/vendor/bootstrap.scss',
+        './src/assets/scss/animate.scss',
+        './src/assets/scss/main.scss'
+    ],
+    sassSourceDev: './src/assets/scss/main.scss',
+    sassSourceDevVendor: [
+        './src/assets/scss/vendor/bootstrap.scss',
+        './src/assets/scss/animate.scss'
+    ]
 }
 
 //  Destionation path
@@ -79,16 +99,42 @@ gulp.task('img', function () {
         .pipe(gulp.dest(dist.imgDist))
 })
 
-// JS
-gulp.task('js', function () {
-    return gulp.src(src.jsSource)
+// JS Build
+gulp.task('js-build', function () {
+    return gulp.src(src.jsSourceBuild)
         .pipe(sourcemaps.init())
         .pipe(babel({
             presets: ['es2015']
         }))
         .pipe(uglify())
-        .pipe(concat('main.js', {newLine: '\n\n\n'}))
-        .pipe(sourcemaps.write('.'))
+        .pipe(concatJS('main.min.js', {newLine: '\n\n\n'}))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest(dist.jsDist))
+        .pipe(browserSync.stream())
+})
+
+// JS Dev Vendor
+gulp.task('js-dev-vendor', function () {
+    return gulp.src(src.jsSourceDevVendor)
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(concatJS('vendor.js', {newLine: '\n\n\n'}))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest(dist.jsDist))
+        .pipe(browserSync.stream())
+})
+
+// JS Dev
+gulp.task('js-dev', ['js-dev-vendor'],function () {
+    return gulp.src(src.jsSourceDev)
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(concatJS('main.js', {newLine: '\n\n\n'}))
+        .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest(dist.jsDist))
         .pipe(browserSync.stream())
 })
@@ -103,9 +149,9 @@ gulp.task('pug', function () {
         .pipe(browserSync.stream())
 })
 
-// SCSS
-gulp.task('scss', function () {
-    return gulp.src(src.sassSource)
+// SCSS Build
+gulp.task('scss-build', function () {
+    return gulp.src(src.sassSourceBuild)
         .pipe(sourcemaps.init())
         .pipe(scss().on('error', scss.logError))
         .pipe(autoprefixer())
@@ -113,7 +159,31 @@ gulp.task('scss', function () {
             console.log('Original Size: ' + details.stats.originalSize)
             console.log('Minified Size: ' + details.stats.minifiedSize)
         }))
-        .pipe(sourcemaps.write('.'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest(dist.cssDist))
+        .pipe(browserSync.stream())
+})
+
+// SCSS Dev Vendor
+gulp.task('scss-dev-vendor', function () {
+    return gulp.src(src.sassSourceDevVendor)
+        .pipe(sourcemaps.init())
+        .pipe(scss().on('error', scss.logError))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest(dist.cssDist))
+})
+
+// SCSS Dev
+gulp.task('scss-dev', ['scss-dev-vendor'], function () {
+    return gulp.src(src.sassSourceDev)
+        .pipe(sourcemaps.init())
+        .pipe(scss().on('error', scss.logError))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest(dist.cssDist))
         .pipe(browserSync.stream())
 })
@@ -124,18 +194,32 @@ gulp.task('clean', function () {
         .pipe(clean())
 })
 
-
-// Create server
-gulp.task('serve', ['font', 'img', 'js', 'pug', 'scss'], function () {
+// Create server for dev
+gulp.task('serve-dev', ['font', 'img', 'js-dev', 'pug', 'scss-dev'], function () {
     browserSync.init({
         notify: false,
         server: './dist'
     })
 
-    gulp.watch(srcWatch[0], ['js'])
+    gulp.watch(srcWatch[0], ['js-dev'])
     gulp.watch(srcWatch[1], ['pug'])
-    gulp.watch(srcWatch[2], ['scss'])
+    gulp.watch(srcWatch[2], ['scss-dev'])
 })
 
-// Default
-gulp.task('default', ['serve'])
+// Create server for build
+gulp.task('serve-build', ['font', 'img', 'js-build', 'pug', 'scss-build'], function () {
+    browserSync.init({
+        notify: false,
+        server: './dist'
+    })
+
+    gulp.watch(srcWatch[0], ['js-build'])
+    gulp.watch(srcWatch[1], ['pug'])
+    gulp.watch(srcWatch[2], ['scss-build'])
+})
+
+// Default dev
+gulp.task('default', ['serve-dev'])
+
+// Build production
+gulp.task('build', ['serve-build'])
